7241b8067d27a66a6888fae27b841997000fbf98
Khalid bin hashim <128578192+khalidHashim1@users.noreply.github.com>
2026-02-22 07:28:41 +0300

    Add AWS ACM certificate resource for domain

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,10 @@
+resource "aws_acm_certificate" "cert" {
+  domain_name       = "khalidhashim.com"
+  validation_method = "DNS"
 
+  subject_alternative_names = ["www.khalidhashim.com"]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
