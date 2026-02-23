64fb3d315103e922af33a7e788f2afdc6c17b8ca
Khalid bin hashim <128578192+khalidHashim1@users.noreply.github.com>
2026-02-23 10:35:02 +0300

    Create backend.tf

---
+++ terraform/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "khalidhashim-terraform-state"
+    key            = "prod/terraform.tfstate"
+    region         = "us-east-1"   # change if different
+    #dynamodb_table = "terraform-locks" # only if you created this
+    encrypt        = true
+  }
+}
