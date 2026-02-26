c8467163d6000abd60cb40f69b2f9dad781f5d81
Khalid bin hashim <128578192+khalidHashim1@users.noreply.github.com>
2026-02-27 01:05:18 +0300

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "khalidhashim-terraform-state"
-    key            = "khalidhashim.com/terraform.tfstate"
+    key = "khalidhashim.com/${terraform.workspace}/terraform.tfstate"
     region         = "us-east-1"   # change if different
     #dynamodb_table = "terraform-locks" # only if you created this
     encrypt        = true
