83487c9d22a3dbaad512638632b01a076ffade32
Khalid bin hashim <128578192+khalidHashim1@users.noreply.github.com>
2026-02-27 01:19:22 +0300

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "khalidhashim-terraform-state"
-    key            = "khalidhashim.com/${terraform.workspace}/terraform.tfstate"
+    key            = "khalidhashim.com/terraform.tfstate" # temporary, overridden by backend-config
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
