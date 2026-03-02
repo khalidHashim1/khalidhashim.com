29a8e3967592aa45861c7750401c8cc598889072
khalidhashim1 <khalidhashim1422@gmail.com>
2026-03-02 04:57:27 +0300

    Update main.tf with latest changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,10 @@
-##############################
-# AWS Provider with alias
-##############################
-provider "aws" {
-  alias  = "website"
-  region = "eu-north-1" # Stockholm
-}
 
 ##############################
 # S3 Bucket (Private)
 ##############################
 resource "aws_s3_bucket" "portfolio" {
   provider = aws.website
-  bucket   = "khalidhashim.com"
+  bucket = "khalidhashim.com"
 
   tags = {
     Name        = "Portfolio Website"
@@ -24,7 +17,7 @@ resource "aws_s3_bucket" "portfolio" {
 ##############################
 resource "aws_s3_bucket_public_access_block" "portfolio" {
   provider = aws.website
-  bucket   = aws_s3_bucket.portfolio.id
+  bucket = aws_s3_bucket.portfolio.id
 
   block_public_acls       = true
   block_public_policy     = true
@@ -36,33 +29,34 @@ resource "aws_s3_bucket_public_access_block" "portfolio" {
 # CloudFront Origin Access Control (OAC)
 ##############################
 resource "aws_cloudfront_origin_access_control" "oac" {
-  provider                          = aws.website
+  provider                         = aws.website
   name                              = "portfolio-oac"
   description                       = "OAC for khalidhashim.com"
-  signing_behavior                  = "always"
-  signing_protocol                  = "sigv4"
-  origin_access_control_origin_type = "s3"
+  signing_behavior                   = "always"
+  signing_protocol                   = "sigv4"
+  origin_access_control_origin_type  = "s3"
 }
 
 ##############################
 # CloudFront Distribution
 ##############################
 resource "aws_cloudfront_distribution" "portfolio" {
-  provider            = aws.website
-  enabled             = true
-  is_ipv6_enabled     = true
-  comment             = "Portfolio CloudFront Distribution"
-  price_class         = "PriceClass_All"
+  provider         = aws.website
+  enabled          = true
+  is_ipv6_enabled  = true
+  comment          = "Portfolio CloudFront Distribution"
+  price_class      = "PriceClass_All"
   wait_for_deployment = true
   default_root_object = "index.html"
 
   aliases = [
     "khalidhashim.com",
+    "www.khalidhashim.com",
   ]
 
   origin {
-    origin_id                = "S3-khalidhashim"
-    domain_name              = aws_s3_bucket.portfolio.bucket_regional_domain_name
+    origin_id   = "S3-khalidhashim"
+    domain_name = aws_s3_bucket.portfolio.bucket_regional_domain_name
     origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
   }
 
@@ -85,9 +79,9 @@ resource "aws_cloudfront_distribution" "portfolio" {
   }
 
   viewer_certificate {
-    acm_certificate_arn            = "arn:aws:acm:us-east-1:665832050840:certificate/0a85c5bc-3c94-41d4-add2-d8b58507511c"
-    ssl_support_method             = "sni-only"
-    minimum_protocol_version       = "TLSv1.2_2021"
+    acm_certificate_arn = aws_acm_certificate.main.arn
+    ssl_support_method  = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2021"
     cloudfront_default_certificate = false
   }
 
@@ -130,13 +124,86 @@ resource "aws_s3_bucket_policy" "portfolio_policy" {
 ##############################
 resource "aws_route53_record" "root" {
   provider = aws.website
-  zone_id  = "Z04058082YY1290VSKWE8"
-  name     = "khalidhashim.com"
-  type     = "A"
+  zone_id = "Z04058082YY1290VSKWE8"
+  name    = "khalidhashim.com"
+  type    = "A"
 
   alias {
     name                   = aws_cloudfront_distribution.portfolio.domain_name
     zone_id                = "Z2FDTNDATAQYW2" # CloudFront hosted zone
     evaluate_target_health = false
   }
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = "www.khalidhashim.com"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.portfolio.domain_name
+    zone_id                = "Z2FDTNDATAQYW2"
+    evaluate_target_health = false
+  }
+}
+
+##############################
+# ACM Certificate (us-east-1)
+##############################
+resource "aws_acm_certificate" "main" {
+  provider          = aws.us
+  domain_name       = "khalidhashim.com"
+  validation_method = "DNS"
+
+  subject_alternative_names = [
+    "www.khalidhashim.com",
+    "*.khalidhashim.com"
+  ]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = {
+    Name = "khalidhashim-main-cert"
+  }
+}
+
+##############################
+# Get Route53 Zone
+##############################
+data "aws_route53_zone" "main" {
+  name         = "khalidhashim.com"
+  private_zone = false
+}
+
+##############################
+# DNS Validation Records
+##############################
+resource "aws_route53_record" "cert_validation" {
+  allow_overwrite = true
+
+  for_each = {
+    for dvo in aws_acm_certificate.main.domain_validation_options :
+    dvo.domain_name => {
+      name   = dvo.resource_record_name
+      type   = dvo.resource_record_type
+      record = dvo.resource_record_value
+    }
+  }
+
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  ttl     = 300
+  records = [each.value.record]
+}
+
+##############################
+# Certificate Validation
+##############################
+resource "aws_acm_certificate_validation" "main" {
+  provider                = aws.us
+  certificate_arn         = aws_acm_certificate.main.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
 }
